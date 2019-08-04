886be5dc6f572b94f2d41b9722d7a56123f82019
Nicolas Cuillery <nicolas.cuillery@gmail.com>
2019-08-04 16:02:58 -0400

    Splashscreen animation

--- lib/splash/splash_scene.dart
+++ lib/splash/splash_scene.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
 
-class SplashScene extends StatelessWidget {
+class SplashScene extends StatefulWidget {
+  @override
+  _SplashSceneState createState() => _SplashSceneState();
+}
+
+class _SplashSceneState extends State<SplashScene> {
+  String currentAnimation = 'In';
+
+  void _handleComplete(String animationName) {
+    if (animationName == 'In') {
+      Future.delayed(Duration(seconds: 1), () {
+        setState(() => currentAnimation = 'Out');
+      });
+    } else if (animationName == 'Out') {
+      Navigator.of(context).pushNamed('/home');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Splash scene', style: TextStyle(color: Colors.white)),
-            RaisedButton(
-              onPressed: () => Navigator.pushNamed(context, '/home'),
-              child: Text('Go to Home'),
-              textColor: Colors.white,
-              color: Colors.redAccent,
-            ),
-          ],
+        child: FlareActor('assets/splash.flr',
+          animation: currentAnimation,
+          callback: _handleComplete,
         ),
       ),
     );
